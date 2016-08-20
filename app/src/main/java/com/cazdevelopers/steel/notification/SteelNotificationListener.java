2f52410bc200ca67ddaa6b3783c835e821c164e9
Corey Woodfield <coreywoodfield@gmail.com>
2016-08-20 13:07:03 -0600

    Testing NotificationListenerService

--- app/src/main/java/com/cazdevelopers/steel/notification/SteelNotificationListener.java
+++ app/src/main/java/com/cazdevelopers/steel/notification/SteelNotificationListener.java
@@ -1,7 +1,11 @@
 package com.cazdevelopers.steel.notification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+
+import com.cazdevelopers.steel.R;
 
 /**
  * Created by coreywoodfield on 8/10/16.
@@ -11,10 +15,18 @@ public class SteelNotificationListener extends NotificationListenerService {
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         super.onNotificationPosted(sbn);
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setSmallIcon(R.drawable.steel_alloy);
+        builder.setContentTitle("You got a notification!");
+        String packageName = sbn.getPackageName();
+        builder.setContentText(String.format("A notification was received by %s", packageName.substring(packageName.lastIndexOf('.') + 1)));
+        ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).notify(1, builder.build());
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
         super.onNotificationRemoved(sbn);
+
     }
 }
