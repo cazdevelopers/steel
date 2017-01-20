a38b97f39b40efacf0da6a1775c90cf17f0b6d8f
Corey Woodfield <coreywoodfield@gmail.com>
2017-01-19 19:58:46 -0700

    Added pending intent to fire when message was sent (or not sent)

--- app/src/main/java/com/cazdevelopers/steel/SteelBroadcastReceiver.java
+++ app/src/main/java/com/cazdevelopers/steel/SteelBroadcastReceiver.java
@@ -1,19 +1,51 @@
 package com.cazdevelopers.steel;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
+import android.telephony.SmsManager;
+import android.util.Log;
 
+import com.cazdevelopers.steel.firebase.SteelFirebaseMessagingService;
 import com.cazdevelopers.steel.notification.SteelNotificationListener;
 
 /**
  * Created by coreywoodfield on 8/26/16.
  */
 public class SteelBroadcastReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            context.startService(new Intent(context, SteelNotificationListener.class));
-        }
-    }
+	private static final String TAG = SteelBroadcastReceiver.class.getSimpleName();
+
+	private static final long[] VIBRATE_PATTERN = new long[]{300, 100, 300, 100, 300};
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+			context.startService(new Intent(context, SteelNotificationListener.class));
+		} else if (intent.getAction().equals(SteelFirebaseMessagingService.MESSAGE_SENT)) {
+			Log.d(TAG, "onReceive: received broadcast");
+			int resultCode = getResultCode();
+			Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+			if (resultCode == Activity.RESULT_OK) {
+				vibrator.vibrate(500);
+			} else {
+				switch (resultCode) {
+					case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+						Log.e(TAG, "onReceive: RESULT_ERROR_GENERIC_FAILURE");
+						break;
+					case SmsManager.RESULT_ERROR_NO_SERVICE:
+						Log.e(TAG, "onReceive: RESULT_ERROR_NO_SERVICE");
+						break;
+					case SmsManager.RESULT_ERROR_NULL_PDU:
+						Log.e(TAG, "onReceive: RESULT_ERROR_NULL_PDU");
+						break;
+					case SmsManager.RESULT_ERROR_RADIO_OFF:
+						Log.e(TAG, "onReceive: RESULT_ERROR_RADIO_OFF");
+						break;
+				}
+				vibrator.vibrate(VIBRATE_PATTERN, -1);
+			}
+		}
+	}
 }
--- app/src/main/java/com/cazdevelopers/steel/firebase/SteelFirebaseMessagingService.java
+++ app/src/main/java/com/cazdevelopers/steel/firebase/SteelFirebaseMessagingService.java
@@ -1,8 +1,11 @@
 package com.cazdevelopers.steel.firebase;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.telephony.SmsManager;
 import android.util.Log;
 
+import com.cazdevelopers.steel.SteelBroadcastReceiver;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -16,6 +19,7 @@ public class SteelFirebaseMessagingService extends FirebaseMessagingService {
 	private static final String TAG = SteelFirebaseMessagingService.class.getSimpleName();
 	public static final String MESSAGE = "message";
 	public static final String NUMBER = "number";
+	public static final String MESSAGE_SENT = "message_sent";
 	private final SmsManager smsManager = SmsManager.getDefault();
 
 	@Override
@@ -30,10 +34,17 @@ public class SteelFirebaseMessagingService extends FirebaseMessagingService {
 	}
 
 	private void sendMessage(ArrayList<String> message, String number) {
-		if (message.size() == 1) {
-			smsManager.sendTextMessage(number, null, message.get(0), null, null);
+		int size = message.size();
+		Intent intent = new Intent(this, SteelBroadcastReceiver.class).setAction(MESSAGE_SENT);
+		if (size == 1) {
+			PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+			smsManager.sendTextMessage(number, null, message.get(0), pendingIntent, null);
 		} else {
-			smsManager.sendMultipartTextMessage(number, null, message, null, null);
+			ArrayList<PendingIntent> intents = new ArrayList<>(size);
+			for (int i = 0; i < size; i++) {
+				intents.add(PendingIntent.getBroadcast(this, i, intent, PendingIntent.FLAG_CANCEL_CURRENT));
+			}
+			smsManager.sendMultipartTextMessage(number, null, message, intents, null);
 		}
 	}
 }
