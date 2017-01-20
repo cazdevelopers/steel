0afe20a92a35150d378e72e1d654d2176a7adf52
Corey Woodfield <coreywoodfield@gmail.com>
2017-01-19 20:01:28 -0700

    Reformatted code to use tabs instead of spaces

--- app/src/main/java/com/cazdevelopers/steel/firebase/SteelFirebaseInstanceIdService.java
+++ app/src/main/java/com/cazdevelopers/steel/firebase/SteelFirebaseInstanceIdService.java
@@ -10,14 +10,14 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
  * Created by coreywoodfield on 8/10/16.
  */
 public class SteelFirebaseInstanceIdService extends FirebaseInstanceIdService {
-    private static final String TAG = SteelFirebaseInstanceIdService.class.getSimpleName();
+	private static final String TAG = SteelFirebaseInstanceIdService.class.getSimpleName();
 
-    @Override
-    public void onTokenRefresh() {
-        Log.d(TAG, "onTokenRefresh: Saving token to sharedpreferences");
-        PreferenceManager.getDefaultSharedPreferences(this)
-                .edit()
-                .putString("token", FirebaseInstanceId.getInstance().getToken())
-                .apply();
-    }
+	@Override
+	public void onTokenRefresh() {
+		Log.d(TAG, "onTokenRefresh: Saving token to sharedpreferences");
+		PreferenceManager.getDefaultSharedPreferences(this)
+						 .edit()
+						 .putString("token", FirebaseInstanceId.getInstance().getToken())
+						 .apply();
+	}
 }
